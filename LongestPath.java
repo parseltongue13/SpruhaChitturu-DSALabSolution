4284f5644c1ded24612170546cdc8dbbcebf9396
parseltongue13 <64133529+parseltongue13@users.noreply.github.com>
2022-01-31 23:10:51 +0530

    Create LongestPath.java

---
+++ LongestPath.java
@@ -0,0 +1,57 @@
+package longestPath;
+
+import java.util.ArrayList;
+
+public class Driver {
+    static class Node {
+        Node left;
+        Node right;
+        int data;
+    }
+
+    static Node newNode(int data) {
+        Node temp = new Node();
+
+        temp.data = data;
+        temp.left = null;
+        temp.right = null;
+
+        return temp;
+    }
+
+    public static ArrayList<Integer> longestPath(Node root) {
+
+        if (root == null) {
+            ArrayList<Integer> output = new ArrayList<>();
+            return output;
+        }
+
+        ArrayList<Integer> right = longestPath(root.right);
+        ArrayList<Integer> left = longestPath(root.left);
+
+        if (right.size() < left.size()) {
+            left.add(root.data);
+        } else {
+            right.add(root.data);
+        }
+        return (left.size() > right.size() ? left : right);
+    }
+
+    public static void main(String[] args) {
+        Node root = newNode(100);
+        root.left = newNode(20);
+        root.right = newNode(130);
+        root.left.left = newNode(10);
+        root.left.right = newNode(50);
+        root.right.left = newNode(110);
+        root.right.right = newNode(140);
+        root.left.left.left = newNode(5);
+
+        ArrayList<Integer> output = longestPath(root);
+        int n = output.size();
+        System.out.print(output.get(n - 1));
+        for (int i = n - 2; i >= 0; i--) {
+            System.out.print(" -> " + output.get(i));
+        }
+    }
+}
